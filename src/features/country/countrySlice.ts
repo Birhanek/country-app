c3601aae8550379641e122ce2fed9f100ea98a7f
Birhanek <birhane.kahsay99@gmail.com>
2023-03-20 20:57:59 +0100

    adjustment on search

--- src/features/country/countrySlice.ts
+++ src/features/country/countrySlice.ts
@@ -32,19 +32,12 @@ export const countrySlice = createSlice({
         }*/
         IncrementFavorite:(state,action:PayloadAction<FavoriteCountry>)=>{
             state.favoriteCountry.push(action.payload.country)
-            state.favoriteCountry.map(country=>country.isFavorite = true)
             state.favoriteCount += action.payload.count
         },
         DecrementFavorite:(state,action:PayloadAction<FavoriteCountry>)=>{
-            //const index:number = state.favoriteCountry.map((favorite)=>favorite.name.official).indexOf(action.payload.country.name.official)
-            // Due to slow performance in large arrays 
             const index:number = state.favoriteCountry.findIndex(favorite=>favorite.name.official === action.payload.country.name.official)
-            const removeFavorite = state.favoriteCountry.find((favorite)=>favorite.name.official === action.payload.country.name.official)
-            if(removeFavorite) removeFavorite.isFavorite = false
             state.favoriteCountry.splice(index,1)
-            state.favoriteCount -= action.payload.count
-           // state.favoriteCountry.filter((favorite)=>)
-          
+            state.favoriteCount -= action.payload.count  
         }
 
     },
@@ -57,7 +50,6 @@ export const countrySlice = createSlice({
         })
         builder.addCase(getAllCountries.fulfilled,(state,action:PayloadAction<CountryInfo[]>) => {
             state.countryState = action.payload
-            state.countryState.map((country)=>country.isFavorite=false) // added to initially the favorite is false
             state.isLoading = false
             state.isError = false
             state.message = 'Data fetched successfully'
