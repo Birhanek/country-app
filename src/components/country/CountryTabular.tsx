e2eac654bb81ddd0b6ba1ad8cca066a77ae7eaa6
Birhanek <birhane.kahsay99@gmail.com>
2023-03-22 09:42:23 +0100

    improvment

--- src/components/country/CountryTabular.tsx
+++ src/components/country/CountryTabular.tsx
@@ -1,7 +1,7 @@
 
 import { Favorite } from '@mui/icons-material';
 import React, { useState } from 'react'
-import { FaAngleRight, FaHeart } from 'react-icons/fa'
+import { FaAngleRight } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
